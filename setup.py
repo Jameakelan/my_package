1f427771a51dff36aba942558c4cb65a45d930c9
Jameakelan <akananyotkeawlamoon2@gmail.com>
2023-05-14 12:13:37 +0700

    change package name

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='my_package',
+    name='mypackage',
     version='0.1',
     packages=find_packages(),
     install_requires=[
         'numpy',
     ],
-    distname='my_package'
 )
