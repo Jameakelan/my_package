812db47b4bb09716e3c1ec31a677791c3fa7c684
Jameakelan <akananyotkeawlamoon2@gmail.com>
2023-05-14 12:24:03 +0700

    build dist

--- setup.py
+++ setup.py
@@ -4,6 +4,10 @@ setup(
     name='mypackage',
     version='0.1',
     packages=find_packages(),
+    description="My Python package",
+    author="Your Name",
+    author_email="your_email@example.com",
+    url="https://github.com/Jameakelan/mypackage",
     install_requires=[
         'numpy',
     ],
